8b2d2c3ea084dc3cd56d23ac5ca177ce33a1969d
gianlukk994 <34285663+gianlukk994@users.noreply.github.com>
2022-01-31 17:19:54 +0100

    Add campaign contract

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minumumContribution;
+    
+    /// Replaced array with map for performance issue, so the map is '0x123dhasd': boolean
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minumumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minumumContribution);
+
+        /// Once an approver call this method will be added to address map '0x123dhasd': true
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
