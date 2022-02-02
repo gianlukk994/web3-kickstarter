fbf8d975148bc1b2668171ef9b88dd610f9884c6
gianlukk994 <34285663+gianlukk994@users.noreply.github.com>
2022-02-02 17:32:17 +0100

    Add header navigation

--- components/header/index.js
+++ components/header/index.js
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import React from "react";
 import { Menu } from "semantic-ui-react";
 
 const Header = () => (
   <Menu style={{ marginTop: "10px" }}>
-    <Menu.Item>CrowdCoin</Menu.Item>
-
+    <Menu.Item>
+      <Link href="/">CrowdCoin</Link>
+    </Menu.Item>
     <Menu.Menu position="right">
-      <Menu.Item>Campaigns</Menu.Item>
-      <Menu.Item>+</Menu.Item>
+      <Menu.Item>
+        <Link href="/">Campaigns</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link href="/campaigns/new">+</Link>
+      </Menu.Item>
     </Menu.Menu>
   </Menu>
 );
